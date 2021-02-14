704425f607ff27b6bc9ab4fdb5cef66014ec33fe
WD <69964898+WD-Leong@users.noreply.github.com>
2021-02-14 11:27:01 +0800

    Add files via upload

---
+++ YOLOv1/train_yolov1_MNIST.py
@@ -0,0 +1,273 @@
+
+import time
+import numpy as np
+import pandas as pd
+import pickle as pkl
+from matplotlib import pyplot as plt
+
+import tensorflow as tf
+from data_preprocess import preprocess_data
+from tf_yolov1 import build_model, format_data, model_loss
+
+def _parse_image(filename):
+    image_string  = tf.io.read_file(filename)
+    image_decoded = \
+        tf.image.decode_jpeg(image_string, channels=3)
+    return tf.cast(image_decoded, tf.float32)
+
+def show_heatmap(
+    image, img_labels, center=False, img_rows=384, img_cols=384):
+    img_w = int(image.shape[0])
+    img_h = int(image.shape[1])
+    
+    tmp_array = np.zeros(
+        [int(img_rows/8), int(img_cols/8)])
+    tmp_object = np.zeros(
+        [int(img_rows/8), int(img_cols/8)])
+        
+    tmp_output = img_labels
+    cen_output = tmp_output[..., 4]
+    cls_output = tmp_output[..., 5:]
+    max_probs  = tf.reduce_max(cls_output, axis=2)
+    
+    if center:
+        tmp_array = np.multiply(cen_output, max_probs)
+    else:
+        tmp_array = max_probs
+    tmp_object = cen_output
+    
+    tmp_array = tf.expand_dims(tmp_array, axis=2)
+    tmp_heatmap = tf.image.resize(
+        tf.expand_dims(tmp_array, axis=0), [img_w, img_h])
+    tmp_heatmap = tf.squeeze(tmp_heatmap, axis=3)
+    
+    tmp_object = tf.expand_dims(tmp_object, axis=2)
+    tmp_center = tf.image.resize(
+        tf.expand_dims(tmp_object, axis=0), [img_w, img_h])
+    tmp_center = tf.squeeze(tmp_center, axis=3)
+    
+    fig, ax = plt.subplots(1)
+    tmp_img = np.array(image, dtype=np.uint8)
+    ax.imshow(tmp_img)
+    tmp = ax.imshow(tmp_heatmap[0], "jet", alpha=0.50)
+    fig.colorbar(tmp, ax=ax)
+    
+    fig.suptitle("Ground Truth Heatmap")
+    fig.savefig("true_heatmap.jpg", dpi=199)
+    plt.close()
+    del fig, ax
+    
+    fig, ax = plt.subplots(1)
+    tmp_img = np.array(image, dtype=np.uint8)
+    ax.imshow(tmp_img)
+    tmp = ax.imshow(tmp_center[0], "jet", alpha=0.50)
+    fig.colorbar(tmp, ax=ax)
+    
+    fig.suptitle("Ground Truth Center Map")
+    fig.savefig("true_center_map.jpg", dpi=199)
+    plt.close()
+    del fig, ax
+    return None
+
+# Training function. #
+def train(
+    train_data, training_loss, model, 
+    batch_size, optimizer, ckpt, ck_manager, 
+    st_step, max_steps, init_lr=1.0e-3, min_lr=1.0e-5, 
+    decay_step=1000, display_step=50, step_save=100, 
+    step_cool=1000, save_loss_file="train_losses.csv"):
+    n_data = len(train_data)
+    
+    start_time = time.time()
+    total_loss = 0.0
+    trend_loss = 0.0
+    
+    tot_reg_loss = 0.0
+    tot_cls_loss = 0.0
+    tot_cen_loss = 0.0
+    model_params = model.trainable_variables
+    for step in range(st_step, max_steps):
+        step_lr = max(init_lr * np.power(
+            0.5, int(step / decay_step)), min_lr)
+        optimizer.lr.assign(step_lr)
+        
+        batch_sample  = np.random.choice(
+            n_data, size=batch_size, replace=False)
+        acc_gradients = [
+            tf.zeros_like(var) for var in model_params]
+        
+        # Loss is computed per image. #
+        acc_losses = 0.0
+        reg_losses = 0.0
+        cls_losses = 0.0
+        cen_losses = 0.0
+        for tmp_idx in batch_sample:
+            image, bbox, class_id, img_dim = \
+                preprocess_data(train_data[tmp_idx])
+            class_id = tf.cast(class_id, tf.float32)
+            
+            label = tf.concat([
+                bbox, tf.expand_dims(class_id, 1)], axis=1)
+            image = tf.expand_dims(image, axis=0)
+            
+            img_pad = [int(image.shape[1]), 
+                       int(image.shape[2])]
+            
+            tmp_labels = format_data(
+                label, img_dim, num_classes, img_pad=img_pad)
+            tmp_image  = 127.5 * (image[0] + 1.0)
+            show_heatmap(tmp_image, tmp_labels, 
+                         img_rows=img_pad[0], img_cols=img_pad[1])
+            
+            with tf.GradientTape() as grad_tape:
+                tmp_output = model(image, training=True)
+                tmp_losses = \
+                    model_loss(tmp_labels, tmp_output)
+                all_losses = \
+                    tmp_losses[0] + tmp_losses[1] + tmp_losses[2]
+            
+            # Accumulate the gradients. #
+            cls_losses += tmp_losses[0]
+            reg_losses += tmp_losses[1]
+            cen_losses += tmp_losses[2]
+            acc_losses += all_losses
+            
+            tmp_gradients = \
+                grad_tape.gradient(all_losses, model_params)
+            acc_gradients = [(acc_grad + grad) for \
+                acc_grad, grad in zip(acc_gradients, tmp_gradients)]
+        
+        # Update the weights. #
+        acc_gradients = [tf.math.divide_no_nan(
+            acc_grad, batch_size) for acc_grad in acc_gradients]
+        optimizer.apply_gradients(
+            zip(acc_gradients, model_params))
+        
+        ckpt.step.assign_add(1)
+        total_loss += acc_losses / batch_size
+        trend_loss += acc_losses / batch_size
+        
+        tot_reg_loss += reg_losses / batch_size
+        tot_cls_loss += cls_losses / batch_size
+        tot_cen_loss += cen_losses / batch_size
+        if (step+1) % display_step == 0:
+            avg_loss = total_loss / display_step
+            
+            avg_reg_loss = tot_reg_loss / display_step
+            avg_cls_loss = tot_cls_loss / display_step
+            avg_cen_loss = tot_cen_loss / display_step
+            avg_reg_loss = avg_reg_loss.numpy()
+            avg_cls_loss = avg_cls_loss.numpy()
+            avg_cen_loss = avg_cen_loss.numpy()
+            
+            total_loss = 0.0
+            tot_reg_loss = 0.0
+            tot_cls_loss = 0.0
+            tot_cen_loss = 0.0
+            
+            elapsed_tm = (time.time() - start_time) / 60.0
+            start_time = time.time()
+            
+            print("Iteration:", str(step+1))
+            print("Learning Rate:", str(optimizer.lr.numpy()))
+            print("Average Loss:", str(round(avg_loss.numpy(), 5)))
+            print("Average Reg Loss:", str(round(avg_reg_loss, 5)))
+            print("Average Cls Loss:", str(round(avg_cls_loss, 5)))
+            print("Average Cen Loss:", str(round(avg_cen_loss, 5)))
+            
+            if (step+1) % step_save == 0:
+                # Save the training losses. #
+                training_loss.append((step+1, avg_loss))
+                
+                df_columns = ["step", "train_loss"]
+                train_loss_df = pd.DataFrame(
+                    training_loss, columns=df_columns)
+                train_loss_df.to_csv(save_loss_file, index=False)
+                
+                # Save the model. #
+                print("")
+                save_path = ck_manager.save()
+                print("Saved model to {}".format(save_path))
+            
+            if (step+1) % step_cool != 0:
+                print("Elapsed Time:", str(elapsed_tm), "mins.")
+                print("-" * 50)
+        
+        if (step+1) % step_cool == 0:
+            avg_trend = trend_loss.numpy() / step_cool
+            trend_loss = 0.0
+            
+            print("Trend Loss:", str(round(avg_trend, 5)))
+            print("Elapsed Time:", str(elapsed_tm), "mins.")
+            print("Cooling GPU for 2 minutes.")
+            
+            time.sleep(120)
+            print("-" * 50)
+    return None
+
+# Load the data. #
+tmp_path = "C:/Users/admin/Desktop/GitHub_Cloned_Repo/yymnist/"
+load_pkl_file = tmp_path + "mnist_objects.pkl"
+with open(load_pkl_file, "rb") as tmp_load:
+    mnist_dataset = pkl.load(tmp_load)
+
+# Define the checkpoint callback function. #
+voc_path = "C:/Users/admin/Desktop/TF_Models/mnist_model/"
+train_loss = voc_path + "mnist_losses_yolo_mobilenetv2.csv"
+ckpt_model = voc_path + "mnist_yolo_mobilenetv2"
+restore_flag = False
+
+# Training Parameters. #
+init_lr = 5.0e-4
+step_cool  = 200
+max_steps  = 1000
+disp_step  = 5
+save_step  = 100
+decay_step = 10000
+
+batch_size = 16
+num_classes = 10
+
+fcos_model = build_model(
+    num_classes, backbone_model="mobilenetv2")
+model_optimizer = tf.optimizers.Adam()
+#model_optimizer = tf.optimizers.SGD(
+#    learning_rate=1.0e-3, momentum=0.9)
+
+#print(fcos_model.summary())
+
+checkpoint = tf.train.Checkpoint(
+    step=tf.Variable(0), 
+    fcos_model=fcos_model, 
+    model_optimizer=model_optimizer)
+ck_manager = tf.train.CheckpointManager(
+    checkpoint, directory=ckpt_model, max_to_keep=1)
+
+if restore_flag:
+    train_loss_df = pd.read_csv(train_loss)
+    training_loss = [tuple(
+        train_loss_df.iloc[x].values) \
+        for x in range(len(train_loss_df))]
+    
+    checkpoint.restore(ck_manager.latest_checkpoint)
+    if ck_manager.latest_checkpoint:
+        print("Model restored from {}".format(
+            ck_manager.latest_checkpoint))
+    else:
+        print("Error: No latest checkpoint found.")
+else:
+    training_loss = []
+st_step = checkpoint.step.numpy().astype(np.int32)
+
+# Training the model. #
+print("")
+print("Training FoveaNet Model with", str(num_classes), 
+      "classes (" + str(st_step) + " iterations).")
+
+train(
+    mnist_dataset, training_loss, 
+    fcos_model, batch_size, model_optimizer, 
+    checkpoint, ck_manager, st_step, max_steps, 
+    init_lr=init_lr, decay_step=decay_step, 
+    display_step=disp_step, step_cool=step_cool, 
+    step_save=save_step, save_loss_file=train_loss)
